59d1fcccf9c7346ba42ae5c22abf7f2603e08cbe
valentine-mario <valentine13400@gmail.com>
2019-11-23 19:33:19 +0100

    added resolvers and services for podcast. Also added other needed components to uswer module

---
+++ src/app/user/podcast-search/podcast-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-podcast-search',
+  templateUrl: './podcast-search.component.html',
+  styleUrls: ['./podcast-search.component.css']
+})
+export class PodcastSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/user/services/podcast.service.ts
+++ src/app/user/services/podcast.service.ts
@@ -18,4 +18,65 @@ export class PodcastService {
       headers: new HttpHeaders({'authorization': "bearer "+authToken}),
     })
   }
+
+  getNewPodcasts(page, limit){
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/getall?page=${page}&per_page=${limit}`)
+  }
+
+  getPodcastFeed(page, limit){
+    let authToken= localStorage.getItem('token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/feed?page=${page}&per_page=${limit}`,  {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  searchPodcast(param, page, limit){
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/search/${param}?page=${page}&per_page=${limit}`)
+  }
+
+  getPodcastByToken(id){
+    let authToken= localStorage.getItem('token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/listen/${id}`, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  mostListenedPodcast(page, limit){
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/mostlistens?page=${page}&per_page=${limit}`)
+  }
+
+  bookmarkPodcast(id){
+    let authToken= localStorage.getItem('token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/bookmark/podcast/${id}`, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  reportPodcast(data, id){
+    let authToken= localStorage.getItem('token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/report/podcast/${id}`, data, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
+
+  checkPodcastBookMark(id){
+    let authToken= localStorage.getItem('token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/bookmark/checkpodcast/${id}`, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
+
+  editPodcast(data, id){
+    let authToken= localStorage.getItem('token')
+    return this.http.post(AppEndpoint.API_ENDPOINT+`/podcast/edit/${id}`, data, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
+
+  deletePodcast(id){
+    let authToken= localStorage.getItem('token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+`/podcast/delete/${id}`, {  
+      headers: new HttpHeaders({'authorization': "bearer "+authToken}),
+    })
+  }
 }
