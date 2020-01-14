7f618b111aabed28e53c85071ffabe786d8d6d8e
valentine-mario <valentine13400@gmail.com>
2020-01-14 13:40:03 +0100

    setup resolvers for comments

---
+++ src/app/user/resolvers/comment.resolvers.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { empty} from 'rxjs'
+import { Resolve } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import {OthersService} from '../services/others.service'
+import {CommentService} from '../services/comment.service'
+
+@Injectable()
+export class GetVideoComment implements Resolve<any> {
+  constructor(private data:CommentService, private reuse:OthersService) {}
+    
+  resolve(route: ActivatedRouteSnapshot){
+    
+   return this.data.getVideoComments(route.paramMap.get('id2'), 1, 6).pipe(catchError((err)=>{
+    this.reuse.errorToast('Error', 'Error connection to the server')
+      return empty();
+  }))
+  }
+}
+
+@Injectable()
+export class GetPodcastComment implements Resolve<any> {
+  constructor(private data:CommentService, private reuse:OthersService) {}
+    
+  resolve(route: ActivatedRouteSnapshot){
+    
+   return this.data.getPodcastComment(route.paramMap.get('id2'), 1, 6).pipe(catchError((err)=>{
+    this.reuse.errorToast('Error', 'Error connection to the server')
+      return empty();
+  }))
+  }
+}
