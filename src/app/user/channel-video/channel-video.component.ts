d9f642fed8c43f39a9acab4fac89de13ed0a0e2f
valentine-mario <valentine13400@gmail.com>
2020-01-15 12:07:26 +0100

    added comments and replies for channel videos

--- src/app/user/channel-video/channel-video.component.ts
+++ src/app/user/channel-video/channel-video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {OthersService} from '../services/others.service'
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
@@ -6,6 +6,7 @@ import {VideoService} from '../services/video.service'
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { Meta, Title } from '@angular/platform-browser';
 import { Location } from '@angular/common';
+import {CommentService} from '../services/comment.service'
 
 @Component({
   selector: 'app-channel-video',
@@ -16,7 +17,7 @@ export class ChannelVideoComponent implements OnInit, OnDestroy{
 closeResult:string
   constructor(private modalService:NgbModal,  private title: Title, private reuse:OthersService, private vidservice:VideoService,
     private meta: Meta, private route:Router, private router:ActivatedRoute, private fb:FormBuilder,
-    private location:Location, private cdRef: ChangeDetectorRef) { 
+    private location:Location, private commentServices:CommentService) { 
       this.router.params.subscribe(params => this.parameter = params.id2)
       route.events.forEach((event) => {
         if(event instanceof NavigationEnd) {
@@ -34,6 +35,8 @@ closeResult:string
     video_link:string
     comments:any
     p:number
+    commentForm:FormGroup
+replyForm:FormGroup
   ngOnInit() {
     setTimeout(() => {
       this.reuse.changeMessage(false)
@@ -59,6 +62,15 @@ closeResult:string
       }, 1000);
 
 
+      //forms for component
+      this.commentForm=this.fb.group({
+        comment:['', Validators.required]
+      })
+
+      this.replyForm=this.fb.group({
+        comment:['', Validators.required],
+
+      })
       this.reportForm=this.fb.group({
         report:['', Validators.required]
       })
@@ -69,8 +81,84 @@ closeResult:string
       
   }
 
-  paginateComment(e){
+  addComment(){
+    var formValue=this.commentForm.value
+    this.commentServices.addVideoComment(formValue, this.video.message.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.comments.message.length < 1 == false
+        this.reuse.successToast('Comment added', '')
+        var push_data={
+          comment:val['message'].comment,
+          created_at:val['message'].created_at,
+          user:{
+            name: val['message'].user.name,
+            token:val['message'].user.token
+          }
+        }
+        this.comments.message.push(push_data)
+        this.commentForm=this.fb.group({
+          comment:['', Validators.required]
+        })
+  
+      }else{
+        this.reuse.errorToast('Error adding comment', val['message'])
+      }
+    })
+  }
 
+  replyComment(a){
+    var formValue=this.replyForm.value
+    this.commentServices.addVideoCommentReplies(formValue, a.id).subscribe(val=>{
+      if(val['code']=="00"){
+        var push_data={
+          comment:val['message'].comment,
+          created_at:val['message'].created_at,
+          user:{
+            name:val['message'].user.name,
+            token:val['message'].user.token
+          }
+        }
+        a.videoreplies.push(push_data)
+        this.reuse.successToast('replied', 'reply added successfully')
+        this.replyForm=this.fb.group({
+          comment:['', Validators.required],
+  
+        })
+      }else{
+          this.reuse.errorToast('error', val['message'])
+      }
+    })
+  }
+
+  deleteReply(a, b){
+    this.commentServices.DeleteVideoReply(b.id).subscribe(val=>{
+      if(val['code']=="00"){
+        a.videoreplies.splice(a.videoreplies.indexOf(b), 1)
+      }else{
+        this.reuse.errorToast("Error", val['message'])
+      }
+    })
+  }
+
+
+  deleteComment(a){
+    this.commentServices.deleteVideoComment(a.id).subscribe(val=>{
+      if(val['code']=="00"){
+        var index=this.comments.message.indexOf(a)
+        this.comments.message.splice(index, 1);
+        this.reuse.successToast('Comment removed', '')
+      }else{
+        this.reuse.errorToast('Error removing comment', val['message'])
+      }
+    })
+  }
+
+
+  paginateComment(e){
+    this.commentServices.getVideoComments(this.parameter, e, 5).subscribe(val=>{
+      this.comments.message=val['message']
+    })
+    this.p=parseInt(e)
   }
   loadNew(){
     this.vidservice.getVideoByToken(this.parameter).subscribe(val=>{
@@ -83,7 +171,9 @@ closeResult:string
         this.bookmark=value['message']
       })
     })
-    
+    this.commentServices.getVideoComments(this.parameter, 1, 5).subscribe(val=>{
+      this.comments=val
+    })
   }
   deleteVideo(){
     this.vidservice.deleteVideo(this.video.message.id).subscribe(val=>{
