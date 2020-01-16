7fdd7babfc8ad58b67379b018f02bfdd7f77ae51
valentine-mario <valentine13400@gmail.com>
2020-01-16 17:09:43 +0100

    added the video bookmark component

--- src/app/user/vid-bookmark-paginate/vid-bookmark-paginate.component.ts
+++ src/app/user/vid-bookmark-paginate/vid-bookmark-paginate.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router'
+import {VideoService}  from '../services/video.service'
+import {OthersService} from '../services/others.service'
+
 
 @Component({
   selector: 'app-vid-bookmark-paginate',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VidBookmarkPaginateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:ActivatedRoute, private route:Router,
+    private videoService:VideoService, private reuse:OthersService) {
+      this.router.params.subscribe(params => this.parameter = params.id)
+     }
+    bookmarks:any;
+    p:number=1
+    parameter:string
 
   ngOnInit() {
+    this.bookmarks=this.router.snapshot.data['video']
+    this.p=parseInt(this.parameter)
+
   }
+  paginate(e){
+     
+    if(e==1){
+      this.route.navigate(['/user/videobookmark'])
+    }else{
+      this.route.navigate(['/user/videobookmark/'+e])
+      this.videoService.viewBookmark(e, 15).subscribe(val=>{
+        this.bookmarks.message=val['message']
+      })
+      this.p=parseInt(e)
+    }
+  }
+  remove(a){
+    this.videoService.bookmarkVideo(a.video.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.bookmarks.message.splice(this.bookmarks.message.indexOf(a), 1)
+      }else{
+        this.reuse.errorToast('Error', val['message'])
+      }
+    })
+  }
+
+
 
 }
