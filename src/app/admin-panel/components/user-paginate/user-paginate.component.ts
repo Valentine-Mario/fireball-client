e731988233659fff01a03f296a09b9986f7c93bf
valentine-mario <valentine13400@gmail.com>
2020-03-12 16:26:20 +0100

    added previos stated feature in the paginate page

--- src/app/admin-panel/components/user-paginate/user-paginate.component.ts
+++ src/app/admin-panel/components/user-paginate/user-paginate.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {UsersService} from '../../services/users.service'
+import {OthersService} from '../../../user/services/others.service'
 
 @Component({
   selector: 'app-user-paginate',
@@ -9,7 +10,8 @@ import {UsersService} from '../../services/users.service'
 })
 export class UserPaginateComponent implements OnInit {
 
-  constructor(private userService:UsersService, private router:Router, private route:ActivatedRoute) {
+  constructor(private userService:UsersService, private router:Router,
+     private route:ActivatedRoute, private reuseable:OthersService) {
     this.route.params.subscribe(params => this.parameter = params.id)
    }
 users:any;
@@ -33,4 +35,53 @@ parameter:string
     this.p=parseInt(a)
     } 
   }
+
+  suspendUser(user){
+    this.userService.suspendUser(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} suspended successfully`);
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].suspended=true;
+      }else{
+        this.reuseable.errorToast('', `error suspending ${user.name}`)
+      }
+    })
+  }
+
+  unsuspendUser(user){
+    this.userService.unsuspendUser(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} unsuspended successfully`);
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].suspended=false;
+      }else{
+        this.reuseable.errorToast('', `error unsuspending ${user.name}`)
+      }
+    })
+  }
+
+  makeAdmin(user){
+    this.userService.makeAdmin(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} successfully made an admin`)
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].isAdmin=true
+      }else{
+        this.reuseable.errorToast('', `error making ${user.name} an admin`)
+      }
+    })
+  }
+
+  removeAdmin(user){
+    this.userService.removeAdmin(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} successfully removed as an admin`)
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].isAdmin=false
+      }else{
+        this.reuseable.errorToast('', `error removing ${user.name} as an admin`)
+      }
+    })
+  }
+
 }
