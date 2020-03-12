5a990af50e19827e4926d7a952d02d5fde832b64
valentine-mario <valentine13400@gmail.com>
2020-03-12 16:23:44 +0100

    added feature to remove admin, add admin, suspend user and unsuspend user

--- src/app/admin-panel/components/home/home.component.ts
+++ src/app/admin-panel/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {OthersService} from '../../../user/services/others.service'
 import {Router, ActivatedRoute} from '@angular/router'
+import {UsersService} from '../../services/users.service'
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,8 @@ import {Router, ActivatedRoute} from '@angular/router'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private reuseable:OthersService, private router:Router, private route:ActivatedRoute) { }
+  constructor(private reuseable:OthersService, private router:Router,
+     private route:ActivatedRoute, private userService:UsersService) { }
 users:any
 p:number
   ngOnInit() {
@@ -23,6 +25,54 @@ p:number
     }else{
       this.router.navigate(['/adminpanel/action/user/'+a])
     }
+  }
+
+  suspendUser(user){
+    this.userService.suspendUser(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} suspended successfully`);
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].suspended=true;
+      }else{
+        this.reuseable.errorToast('', `error suspending ${user.name}`)
+      }
+    })
+  }
+
+  unsuspendUser(user){
+    this.userService.unsuspendUser(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} unsuspended successfully`);
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].suspended=false;
+      }else{
+        this.reuseable.errorToast('', `error unsuspending ${user.name}`)
+      }
+    })
+  }
+
+  makeAdmin(user){
+    this.userService.makeAdmin(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} successfully made an admin`)
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].isAdmin=true
+      }else{
+        this.reuseable.errorToast('', `error making ${user.name} an admin`)
+      }
+    })
+  }
+
+  removeAdmin(user){
+    this.userService.removeAdmin(user.id).subscribe(val=>{
+      if(val['code']=="00"){
+        this.reuseable.infoToast('', `${user.name} successfully removed as an admin`)
+        var index= this.users.message.findIndex(i => i.id === user['id'])
+        this.users.message[index].isAdmin=false
+      }else{
+        this.reuseable.errorToast('', `error removing ${user.name} as an admin`)
+      }
+    })
   }
 
 }
--- src/app/admin-panel/services/users.service.ts
+++ src/app/admin-panel/services/users.service.ts
@@ -15,4 +15,32 @@ export class UsersService {
       headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
     })
   }
+
+  makeAdmin(id){
+    let authToken= localStorage.getItem('admin-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/admin/makeadmin/'+id, {
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
+
+  removeAdmin(id){
+    let authToken= localStorage.getItem('admin-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/admin/removeadmin/'+id, {
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
+
+  suspendUser(id){
+    let authToken= localStorage.getItem('admin-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/admin/suspend/'+id, {
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
+
+  unsuspendUser(id){
+    let authToken= localStorage.getItem('admin-token')
+    return this.http.get(AppEndpoint.API_ENDPOINT+'/admin/unsuspend/'+id, {
+      headers: new HttpHeaders({'authorization': "bearer "+authToken, 'Content-Type': 'application/json'}),
+    })
+  }
 }
